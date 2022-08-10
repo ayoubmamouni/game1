14ea8882718d4c3ecb055721b3371aa04b6a931b
Ayoub Mamouni <ayoub.mamouni@yandex.com>
2022-08-10 19:57:55 +0100

    fix camera movement

--- build/code0.js
+++ build/code0.js
@@ -1,4 +1,10 @@
 gdjs.Untitled_32sceneCode = {};
+gdjs.Untitled_32sceneCode.GDNewSpriteObjects1= [];
+gdjs.Untitled_32sceneCode.GDNewSpriteObjects2= [];
+gdjs.Untitled_32sceneCode.GDNewTiledSpriteObjects1= [];
+gdjs.Untitled_32sceneCode.GDNewTiledSpriteObjects2= [];
+gdjs.Untitled_32sceneCode.GDNewTiledSprite2Objects1= [];
+gdjs.Untitled_32sceneCode.GDNewTiledSprite2Objects2= [];
 
 gdjs.Untitled_32sceneCode.conditionTrue_0 = {val:false};
 gdjs.Untitled_32sceneCode.condition0IsTrue_0 = {val:false};
@@ -6,11 +12,35 @@ gdjs.Untitled_32sceneCode.condition0IsTrue_0 = {val:false};
 
 gdjs.Untitled_32sceneCode.eventsList0 = function(runtimeScene) {
 
+{
+
+
+
+}
+
+
+{
+
+
+{
+gdjs.copyArray(runtimeScene.getObjects("NewSprite"), gdjs.Untitled_32sceneCode.GDNewSpriteObjects1);
+{gdjs.evtTools.camera.setCameraX(runtimeScene, gdjs.evtTools.common.lerp(gdjs.evtTools.camera.getCameraX(runtimeScene, "", 0), (( gdjs.Untitled_32sceneCode.GDNewSpriteObjects1.length === 0 ) ? 0 :gdjs.Untitled_32sceneCode.GDNewSpriteObjects1[0].getPointX("")), 0.05), "", 0);
+}}
+
+}
+
+
 };
 
 gdjs.Untitled_32sceneCode.func = function(runtimeScene) {
 runtimeScene.getOnceTriggers().startNewFrame();
 
+gdjs.Untitled_32sceneCode.GDNewSpriteObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDNewSpriteObjects2.length = 0;
+gdjs.Untitled_32sceneCode.GDNewTiledSpriteObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDNewTiledSpriteObjects2.length = 0;
+gdjs.Untitled_32sceneCode.GDNewTiledSprite2Objects1.length = 0;
+gdjs.Untitled_32sceneCode.GDNewTiledSprite2Objects2.length = 0;
 
 gdjs.Untitled_32sceneCode.eventsList0(runtimeScene);
 return;
